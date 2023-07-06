7b6bdd4beb9f85530364ad8f10f3fd31f897ef5d
Ppp17 <1462439130@qq.com>
2023-07-06 20:14:24 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      {/* <Login></Login> */}
+      <Register></Register>
+    </div>
+  );
 }
 
 export default App;
