8b71369fc5ce87774fd567c6d82a3750c32d56d3
Ppp17 <1462439130@qq.com>
2023-07-13 16:22:56 +0800

    添加share 登录接口

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import Stories from "../../components/stories/Stories";
 import Posts from "../../components/posts/Posts";
 import "./home.scss";
-import Post from "../../components/post/Post";
-
+import Share from "../../components/share/share";
 const Home = () => {
   return (
     <div className="home">
       <Stories></Stories>
+      <Share />
       <Posts></Posts>
     </div>
   );
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import "./register.scss";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [err, setErr] = useState(null);
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/auth/register", inputs);
+    } catch (err) {
+      setErr(err);
+      console.log(err.response.data);
+    }
+  };
   return (
     <div className="register">
       <div className="card">
@@ -19,12 +39,33 @@ const Register = () => {
         </div>
         <div className="right">
           <h1>Register</h1>
-          <form action="">
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email " />
-            <input type="password" placeholder="Password " />
-            <input type="text" placeholder="Name " />
-            <button>Register</button>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email "
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password "
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Name "
+              name="name"
+              onChange={handleChange}
+            />
+            {err && err.response.data}
+            <button onClick={handleClick}>Register</button>
           </form>
         </div>
       </div>
